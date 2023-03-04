076989d8b36a8c1d6a074fe38d2f4f151478ebd8
eliasfrigard <elias_frigard@hotmail.com>
2023-03-04 18:40:32 +0200

    adding color palette

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <div className='w-full h-screen flex justify-center items-center text-6xl underline font-bold'>
-      Hello Veera Kuisma!
+    <div className='bg-black container w-full h-screen flex flex-col justify-center items-center px-32 font-medium text-primary'>
+      <div>
+        <div className='text-[12rem] leading-none'>Veera</div>
+        <div className='text-[12rem] leading-none ml-36 '>
+          Kuisma
+          <div className='text-2xl w-full flex mt-3 ml-4'>Musician & Composer</div>
+        </div>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,22 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#F2F2F2',
+        black: '#000000',
+        matcha: {
+          800: '#3c4c3f',
+          500: '#667c6e',
+          400: '#8eb59c',
+          300: '#b3d3af',
+          200: '#d9ead3',
+        },
+      },
+    },
+    container: {
+      center: true,
+    },
   },
   plugins: [],
 }
