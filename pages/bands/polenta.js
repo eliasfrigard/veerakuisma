01a2f5f325e979eb35f5a8630dc424654c3cd48a
Elias Frigård <elias.frigard@tvtools.fi>
2023-09-30 18:32:23 +0300

    fixes

--- pages/bands/polenta.js
+++ pages/bands/polenta.js
@@ -1,12 +1,8 @@
-import Link from 'next/link'
-import Image from 'next/image'
 import Layout from '../components/Layouts/default'
-import AnimateIn from '../components/AnimateIn'
 
 export default function Home() {
   return (
     <Layout>
-
     </Layout>
   )
 }
