fcd07e54a9cf2e35b01fb6001d57d3d8f9eb3313
Elias Frigård <elias.frigard@tvtools.fi>
2024-01-28 21:01:23 +0200

    Update padding in about and bands pages

--- pages/about.js
+++ pages/about.js
@@ -71,7 +71,7 @@ const About = ({
             />
           </AnimateIn>
 
-          <AnimateIn threshold={0} className='text-center md:text-justify leading-[2rem] tracking-wide font-sans font-medium z-10 px-6 md:px-10 pt-1 lg:pt-0'>
+          <AnimateIn threshold={0} className='text-center md:text-justify leading-[2rem] tracking-wide font-sans font-medium z-10 px-3 md:px-10 pt-1 lg:pt-0'>
             <TextLayout text={biography} />
           </AnimateIn>
         </div>
