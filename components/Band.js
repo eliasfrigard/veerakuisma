b24ddbc385e8d3a77d20d5c8549bf9bc1e174088
Elias Frigård <elias.frigard@mediatailor.fi>
2024-10-25 12:58:18 +0300

    is mounted in iconHandler

--- components/Band.js
+++ components/Band.js
@@ -8,13 +8,15 @@ import IconHandler from './IconHandler'
 import TextLayout from './TextLayout'
 
 const Band = ({ name, image, description, spotify, email, youTube, website, instagram, facebook }) => {
+  const [link] = React.useState(`/bands/${name.toLowerCase()}`)
+
   return (
     <AnimateIn
       slideDirection='right'
       className='relative centerContent flex-col rounded-xl overflow-hidden shadow-lg mx-4 hover:scale-105 lg:opacity-90 lg:hover:opacity-100'
     >
       <Link
-        href={`/bands/${name.toLowerCase()}`}
+        href={link}
         className={`w-full h-full flex flex-col justify-center items-center cursor-pointer`}
       >
         <Image alt={name} src={image} fill className={`object-cover object-center`} />
@@ -23,23 +25,21 @@ const Band = ({ name, image, description, spotify, email, youTube, website, inst
 
         <div className='w-full max-w-3xl text-primary-100 my-10 lg:my-16 px-4'>
           <div className='w-full flex flex-col justify-center items-center'>
-            <div>
-              <div className='flex flex-col centerContent gap-5'>
-                <h1
-                  className={`text-3xl md:text-4xl drop-shadow-2xl leading-normal text-center tracking-wider font-khorla mx-12`}
-                >
-                  {name}
-                </h1>
-                <IconHandler
-                  className='text-accent-500 gap-6 text-xl'
-                  email={email}
-                  spotify={spotify}
-                  youTube={youTube}
-                  website={website}
-                  instagram={instagram}
-                  facebook={facebook}
-                />
-              </div>
+            <div className='flex flex-col centerContent gap-5'>
+              <h1
+                className={`text-3xl md:text-4xl drop-shadow-2xl leading-normal text-center tracking-wider font-khorla mx-12`}
+              >
+                {name}
+              </h1>
+              <IconHandler
+                className='text-accent-500 gap-6 text-xl'
+                email={email}
+                spotify={spotify}
+                youTube={youTube}
+                website={website}
+                instagram={instagram}
+                facebook={facebook}
+              />
             </div>
 
             <div className='border-b border-primary-100 w-3/4 opacity-10 my-6 lg:my-8'></div>
@@ -54,13 +54,6 @@ const Band = ({ name, image, description, spotify, email, youTube, website, inst
             </div>
           </div>
         </div>
-
-        {/* <Link
-        href={`/bands/${name.toLowerCase()}`}
-        className={`w-full min-h-16 bg-primary-900 opacity-80 uppercase font-bold font-khorla text-primary-50 centerContent cursor-pointer hover:opacity-100 duration-100 tracking-wider text-center p-4 z-10`}
-      >
-        {`Read more about ${name}`}
-      </Link> */}
       </Link>
     </AnimateIn>
   )
